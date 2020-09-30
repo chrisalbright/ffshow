257410f54eba9830fc1a84c48e074916a62c23c5
Chris Albright <calbright@cj.com>
2020-09-29 18:11:19 -0700

    404 when movie doesn't exist

--- src/main/java/com/chrisalbright/ffshow/controller/MovieController.java
+++ src/main/java/com/chrisalbright/ffshow/controller/MovieController.java
@@ -27,19 +27,29 @@ public class MovieController {
   @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiOperation(value = "Returns a single movie, given an Id")
   public Mono<Movie> getMovieById(@PathVariable Integer id) {
-    return movieService.getMovieById(id);
+    return movieService
+        .getMovieById(id)
+        .switchIfEmpty(Mono.error(new MovieNotFoundException()));
   }
 
   @GetMapping(path = "/{id}/showtimes", produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiOperation(value = "Returns all show times for a given movie")
   public Flux<ShowTime> getMovieShowTimes(@PathVariable Integer id) {
-    return Flux.from(movieService.getMovieById(id)).flatMap(movie -> Flux.fromIterable(movie.getShowTimes()));
+    return Flux
+        .from(movieService.getMovieById(id))
+        .switchIfEmpty(Mono.error(new MovieNotFoundException()))
+        .flatMap(movie -> Flux.fromIterable(movie.getShowTimes()));
   }
 
   @PostMapping(path = "/{id}/showtimes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiOperation(value = "Adds a new show time for a given movie")
   public Mono<ShowTime> addNewShowtime(@PathVariable Integer id, @RequestBody ShowTime showTime) {
-    return movieService.getMovieById(id).map(showTime::withMovie).flatMap(showTimeService::addShowTime);
+    return
+        movieService
+        .getMovieById(id)
+            .switchIfEmpty(Mono.error(new MovieNotFoundException()))
+            .map(showTime::withMovie)
+            .flatMap(showTimeService::addShowTime);
   }
 
 }
